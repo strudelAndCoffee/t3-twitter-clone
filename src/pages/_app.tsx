8d6b6fb10685ca99018ed77528a84433d3b3d86d
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2024-03-21 17:04:33 -0500

    start homepage, add sign in/out

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,12 @@
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
-import { Inter } from "next/font/google";
 
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
-
-const inter = Inter({
-  subsets: ["latin"],
-  variable: "--font-sans",
-});
+import Head from "next/head";
+import { SideNav } from "~/components/SideNav";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -18,9 +14,20 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
-      <main className={`font-sans ${inter.variable}`}>
-        <Component {...pageProps} />
-      </main>
+      <Head>
+        <title>Twitter Clone</title>
+        <meta
+          name="description"
+          content="This is a Twitter Clone by Web Dev Simplified"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="container mx-auto flex items-start">
+        <SideNav />
+        <div className="min-h-screen flex-grow border-x">
+          <Component {...pageProps} />
+        </div>
+      </div>
     </SessionProvider>
   );
 };
